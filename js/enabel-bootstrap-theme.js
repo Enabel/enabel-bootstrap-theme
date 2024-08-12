419cad53a50e44d63bce8466769ea059104a4036
Damien Lagae <damienlagae@gmail.com>
2024-08-12 15:22:32 +0200

    💄 Switch font-awesome js to css

--- js/enabel-bootstrap-theme.js
+++ js/enabel-bootstrap-theme.js
@@ -1,15 +1,5 @@
 import '../scss/enabel-bootstrap-theme.scss';
 
-// Font Awesome icons
-import { library, dom } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { far } from '@fortawesome/free-regular-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-
-library.add(fas, far, fab)
-
-dom.i2svg()
-
 import bootstrap from "bootstrap/dist/js/bootstrap.bundle.min";
 global.bootstrap = bootstrap;
 
